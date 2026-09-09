b853d256be129eea24f2ac993df6334a34627c20
ryuuujh <dbdbdb1127@gmail.com>
2026-09-09 11:38:56 +0900

    로또v1

--- lotto.py
+++ lotto.py
@@ -1,6 +1,8 @@
 # random 모듈을 이용해서 1~45 중 중복 없는 번호 6개를 뽑고
 # 자료 구조 set, 버튼을 누르면 5세트를 한번에 생성 (list는 중복/ set은 중복 안됨)
 # datetime 으로 생성 시간도 함께 보여준다
+# 로또 v1
+# 로또 v2
 
 import streamlit as st
 import random
